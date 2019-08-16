38babbd6b514d71a433a06a263d02d378c669d16
G1n7oki <G1n7oki@163.com>
2019-08-16 17:58:18 +0800

    增加路由模块

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import store from './store'
 import { Provider } from 'react-redux'
 import { GlobalStyle } from './style'
 import { Iconfont } from './statics/iconfont/iconfont'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Provider store={store}>
-          <Header />
+           <Router>
+             <Header />
+             <Route path="/" exact component={Home}/>
+             <Route path="/detail" component={Detail}/>
+           </Router>
         </Provider>
         <Iconfont />
         <GlobalStyle />
